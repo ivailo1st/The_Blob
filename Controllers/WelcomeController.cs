0e43f757bfc977909ffd0da412e061940a3c4af2
Ivailo1st <eaaivge@students.eaaa.dk>
2020-11-28 22:39:11 +0100

    Added a few comments for the curious ones; Added some style to password inputs;

--- Controllers/WelcomeController.cs
+++ Controllers/WelcomeController.cs
@@ -34,14 +34,22 @@ namespace The_Blob.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Signup([Bind("user,character")] Combiner combiner)
         {
+            //Variable that checks whether the email provided already exists in the database
+            //This is done by using a lambda function that goes through each user email in the database
+            //and compares it with the given one.
+
             bool result = _context.User.Where(x => x.Email == combiner.user.Email).Any();
             if (ModelState.IsValid && !result)
             {
+                //Local Variables that serve as a way to assign specific values to the raw sql query after them
+                //Each one represents a different given value
                 SqlParameter param1 = new SqlParameter("@UserEmail", combiner.user.Email);
                 SqlParameter param2 = new SqlParameter("@UserPassword", combiner.user.Password);
                 SqlParameter param3 = new SqlParameter("@UserConfirmPassword", combiner.user.ConfirmPassword);
                 SqlParameter param4 = new SqlParameter("@CharacterName", combiner.character.Name);
 
+                //A dbcontext function that is cable of executing raw sql code in mvc
+                //Here it executes a stored procedure using the variables from before
                 _context.Database.ExecuteSqlRaw("createNewUser @UserEmail, @UserPassword, @UserConfirmPassword, @CharacterName", param1,param2,param3,param4);
                 await _context.SaveChangesAsync();
                 return RedirectToAction("Index", "Welcome");

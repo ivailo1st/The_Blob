14d6ad9ff6a48d74bd3a4d4264b583b84be05b4c
Ivailo1st <eaaivge@students.eaaa.dk>
2020-11-30 10:35:17 +0100

    Api Inclusion;

---
+++ Controllers/CharacterAPIController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using The_Blob.Data;
+using The_Blob.Models;
+
+namespace The_Blob.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharacterAPIController : ControllerBase
+    {
+        private readonly BlobContext _context;
+
+        public CharacterAPIController(BlobContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/CharacterAPI
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Character>>> GetCharacter()
+        {
+            return await _context.Character.ToListAsync();
+        }
+
+        // GET: api/CharacterAPI/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Character>> GetCharacter(int id)
+        {
+            var character = await _context.Character.FindAsync(id);
+
+            if (character == null)
+            {
+                return NotFound();
+            }
+
+            return character;
+        }
+
+        // PUT: api/CharacterAPI/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCharacter(int id, Character character)
+        {
+            if (id != character.CharacterId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(character).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CharacterExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/CharacterAPI
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Character>> PostCharacter(Character character)
+        {
+            _context.Character.Add(character);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCharacter", new { id = character.CharacterId }, character);
+        }
+
+        // DELETE: api/CharacterAPI/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Character>> DeleteCharacter(int id)
+        {
+            var character = await _context.Character.FindAsync(id);
+            if (character == null)
+            {
+                return NotFound();
+            }
+
+            _context.Character.Remove(character);
+            await _context.SaveChangesAsync();
+
+            return character;
+        }
+
+        private bool CharacterExists(int id)
+        {
+            return _context.Character.Any(e => e.CharacterId == id);
+        }
+    }
+}

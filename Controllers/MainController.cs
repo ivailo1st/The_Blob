a27c234495ac2c6c113948914b58be46092bc3bd
GabiRod <rodickova.gabriela@gmail.com>
2020-11-30 14:25:51 +0100

    Create MainController.cs

---
+++ Controllers/MainController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using The_Blob.Models;
+using The_Blob.Infrastructure;
+
+namespace The_Blob.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Gets data assigned to session key and sends it to the view
+            User user = HttpContext.Session.GetJson<User>("User");
+            return View(user);
+        }
+    }
+}

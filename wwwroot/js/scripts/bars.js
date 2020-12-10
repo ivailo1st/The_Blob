41132f9484ec8a913423fa3cd4290c22331996cd
Spacehater09 <Spacehater09@gmail.com>
2020-12-10 15:56:58 +0100

    45

--- wwwroot/js/scripts/bars.js
+++ wwwroot/js/scripts/bars.js
@@ -40,7 +40,6 @@ function ifAsleep() {
         else if ((data.hunger >= 1 || data.sleep >= 1 || data.fun >= 1) && (data.hunger < 30 || data.sleep < 30 || data.fun < 30)) {
           document.getElementById("js-face").style.backgroundImage = "none";
           document.getElementById("js-face").style.backgroundImage = "url('/pics/faces/angry.svg')";
-          sendEmail()
         }
       }
     });
@@ -71,7 +70,6 @@ function goToSleep() {
         else if ((data.hunger >= 1 || data.sleep >= 1 || data.fun >= 1) && (data.hunger < 30 || data.sleep < 30 || data.fun < 30)) {
           document.getElementById("js-face").style.backgroundImage = "none";
           document.getElementById("js-face").style.backgroundImage = "url('/pics/faces/angry.svg')";
-          sendEmail()
         }
       }
       else {
@@ -153,10 +151,6 @@ function updateTime() {
           document.getElementById("js-face").style.backgroundImage = "url('/pics/faces/angry.svg')";
           sendEmail();
         }
-      } else {
-        document.getElementById("bottomSleepIcon").style.filter = "brightness(0.75)";
-        document.getElementById("js-face").style.backgroundImage = "none";
-        document.getElementById("js-face").style.backgroundImage = "url('/pics/faces/sleeping.svg')";
       }
 
       let newHunger;
@@ -177,6 +171,7 @@ function updateTime() {
           newSleep,
           newFun,
           newLogDate
+
         ]
         console.log(updateBars)
         fetch("api/CharacterAPI/", {

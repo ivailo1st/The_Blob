0bf939e07ee1ea27cd263dce249e9e359f19dfa1
Ivaylo Georgiev <ivailo2nd@gmail.com>
2020-12-10 15:19:09 +0100

    Improved  addItem func;

--- wwwroot/js/scripts/store.js
+++ wwwroot/js/scripts/store.js
@@ -29,84 +29,86 @@ function closeItemSignal() {
 }
 
 function addItem(itemName, itemPercent, itemURL, itemPrice, charID) {
-  //Fridge Item Logic
-  let addQuery = {
-    foodName: itemName,
-    hungerPercentage: itemPercent,
-    quantity: 1,
-    imageURL: itemURL
-  }
-  //Fetch for Finding a Fridge Item
-  fetch("api/fridgeapi/" + itemName)
-    .then(response => response.json())
-      .then(data => {
-        console.log(data)
-      let fridgeID = data.fridgeId
-      //Fetch for finding CharacterFridge item
-      fetch("api/CharacterFridgeAPI/" + fridgeID).then(response => response.json())
-        .then(data => {
-          if ((data.characterId != null) && (data.fridgeId != null)) {
-            //Fetch for Updating a Fridge Item
-            fetch("api/fridgeapi", {
-              method: "PUT",
-              headers: { "Content-Type": "application/json", "Accept": "application/json" },
-              body: JSON.stringify(addQuery)
+    //Currency Deduction
+    let userCurrency = parseInt(document.getElementById("textCurrency").innerHTML);
+    let newCurrency = userCurrency - itemPrice;
+    console.log(newCurrency);
+    if (newCurrency >= 0) {
+        //Fetch for Updating Character Currency
+        fetch("api/characterapi/" + newCurrency, {
+            method: "PATCH",
+            headers: { "content-type": "application/json", "accept": "application/json" }
 
-            })
-              .then(response => response.json())
-              .then(data => console.log(data)).catch(err => console.log(err));
-          }
-          else {
-            //Fetch for Creating a new Fridge Item
-            fetch("api/fridgeapi", {
-              method: "POST",
-              headers: { "Content-Type": "application/json", "Accept": "application/json" },
-              body: JSON.stringify(addQuery)
+        })
+            .then(response => response.json())
+            .then(json => console.log(json)).catch(err => console.log(err));
+        document.getElementById("textCurrency").innerHTML = newCurrency;
+        addItemSignal();
 
-            })
-              .then(response => response.json())
-              .then(data => {
-                fetch("api/fridgeapi/" + itemName)
-                  .then(response => response.json())
-                  .then(data => {
-                    console.log(data.fridgeId);
+        //Fridge Item Logic
+        let addQuery = {
+            foodName: itemName,
+            hungerPercentage: itemPercent,
+            quantity: 1,
+            imageURL: itemURL
+        }
 
-                    let junctionQuery = {
-                      characterId: charID,
-                      character: null,
-                      fridgeId: data.fridgeId,
-                      fridge: null
-                    }
-                    //Fetch for Creating new CharacterFridge item
-                    fetch("api/CharacterFridgeAPI", {
-                      method: "POST",
-                      headers: { "Content-Type": "application/json", "Accept": "application/json" },
-                      body: JSON.stringify(junctionQuery)
+        //Fetch for Finding a Fridge Item
+        fetch("api/fridgeapi/" + itemName)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                let fridgeID = data.fridgeId
+                //Fetch for finding CharacterFridge item
+                fetch("api/CharacterFridgeAPI/" + fridgeID).then(response => response.json())
+                    .then(data => {
+                        if ((data.characterId != null) && (data.fridgeId != null)) {
+                            //Fetch for Updating a Fridge Item
+                            fetch("api/fridgeapi", {
+                                method: "PUT",
+                                headers: { "Content-Type": "application/json", "Accept": "application/json" },
+                                body: JSON.stringify(addQuery)
 
-                    })
-                  });
-              });
-          }
-        });
-    });
+                            })
+                                .then(response => response.json())
+                                .then(data => console.log(data)).catch(err => console.log(err));
+                        }
+                        else {
+                            //Fetch for Creating a new Fridge Item
+                            fetch("api/fridgeapi", {
+                                method: "POST",
+                                headers: { "Content-Type": "application/json", "Accept": "application/json" },
+                                body: JSON.stringify(addQuery)
 
-  //Currency Deduction
-  let userCurrency = parseInt(document.getElementById("textCurrency").innerHTML);
-  let newCurrency = userCurrency - itemPrice;
-  console.log(newCurrency);
-  if (newCurrency >= 0) {
-    //Fetch for Updating Character Currency
-    fetch("api/characterapi/" + newCurrency, {
-      method: "PATCH",
-      headers: { "content-type": "application/json", "accept": "application/json" }
+                            })
+                                .then(response => response.json())
+                                .then(data => {
+                                    fetch("api/fridgeapi/" + itemName)
+                                        .then(response => response.json())
+                                        .then(data => {
+                                            console.log(data.fridgeId);
 
-    })
-      .then(response => response.json())
-      .then(json => console.log(json)).catch(err => console.log(err));
-    document.getElementById("textCurrency").innerHTML = newCurrency;
-    addItemSignal();
-  }
-  else {
-    alert("Not Enough Money");
-  }
+                                            let junctionQuery = {
+                                                characterId: charID,
+                                                character: null,
+                                                fridgeId: data.fridgeId,
+                                                fridge: null
+                                            }
+                                            //Fetch for Creating new CharacterFridge item
+                                            fetch("api/CharacterFridgeAPI", {
+                                                method: "POST",
+                                                headers: { "Content-Type": "application/json", "Accept": "application/json" },
+                                                body: JSON.stringify(junctionQuery)
+
+                                            })
+                                        });
+                                });
+                        }
+                    });
+            });
+    }
+    else {
+        alert("Not Enough Money");
+    }
+  
 }
